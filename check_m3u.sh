3595224b50ba4a0234b6dd23975e8e3a8d5c2511
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2025-08-17 22:26:47 +0330

    6F 75 50 98 BB FF 72 5B 6F A3 F9 37

--- check_m3u.sh
+++ check_m3u.sh
@@ -1,32 +1,19 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-# 1. Rename .m3u files: replace spaces with underscores
-for f in *.m3u; do
-  mv -- "$f" "${f// /_}"
-done
+# replace spaces in files with an underline
+for i in *.m3u ; do sed -i 's/ /_/g' $i ; done
 
-# 2. Prepare a temporary directory
-tmpdir=$(mktemp -d)
-cleanup() { rm -rf "$tmpdir"; }
-trap cleanup EXIT
+# check for none 200 response coded links
+for i in *.m3u ; do for j in $(cat $i) ; do wget -S --spider -q -t 1 -T 1 --max-redirect 0 $j 2>&1 | grep "HTTP/" | awk '{print $2}' | (cat ; echo $j ;) | paste -s -d " " >> A-$i ; echo -e "$i - $j" ; done ; done
 
-# 3. Process each .m3u file in parallel
-for infile in *.m3u; do
-  # Prefix output files with "200-"
-  outfile="200-${infile}"
-  {
-    echo "#EXTM3U"
-    awk 'BEGIN {RS="\r?\n"; ORS="\n"} /^#/ {info=$0; next} {print info "\n" $0}' "$infile" \
-      | paste - - \
-      | while IFS=$'\t' read -r info url; do
-          status=$(curl -s -o /dev/null -w "%{http_code}" --max-time 5 --head --location-trusted --url "$url")
-          if [[ $status -eq 200 ]]; then
-            echo "${info//_/ }"
-            echo "$url"
-          fi
-        done
-  } > "$outfile" &
-done
+# find links that have 200 response code and remove others
+for i in A-*.m3u ; do cat $i | grep -B1 "200 " | sed 's/200 //g' | awk 'length>3' | grep -A1 "#" > A$i ; done
 
-wait
+# convert underlines in stream titles back to spaces
+for i in *.m3u ; do sed -i '/#/s/_/ /g' $i ; done
+
+# remove extra fluff from file names
+#for i in AA-*.m3u ; do mv $i $(echo $i | sed 's/AA-//') ; done
+
+# make the m3u files proper again by adding the header
+for i in *.m3u ; do sed -i '1s/^/#EXTM3U\n/' $i ; done
